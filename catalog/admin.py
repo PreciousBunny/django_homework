c084f47e81cbd0b686737ecc9cfcc4ab26b82f4e
Danila <icy.calmness@gmail.com>
2023-09-06 04:31:33 +0300

    create admin panel, and redact language in settings.py

--- catalog/admin.py
+++ catalog/admin.py
@@ -7,11 +7,13 @@ from catalog.models import Product, Category
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ('id', 'name', 'price', 'category')
-    search_fields = ('name', 'description')
+    list_display = ('id', 'name', 'price', 'category', 'is_active',)
+    search_fields = ('name', 'description',)
     list_filter = ('category',)
 
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
-    list_display = ('id', 'name')
+    list_display = ('id', 'name', 'description',)
+    search_fields = ('name', 'description',)
+    list_filter = ('name',)

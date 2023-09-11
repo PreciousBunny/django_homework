4c93e9425d432772ff5481443e447f1e11e86e2f
Danila <icy.calmness@gmail.com>
2023-09-11 06:02:42 +0300

    correct FBV in CBV and create CRUD for model Product

--- catalog/urls.py
+++ catalog/urls.py
@@ -8,7 +8,28 @@ app_name = CatalogConfig.name
 
 urlpatterns = [
     path('', index, name='home'),  # http://127.0.0.1:8000/
-    path('contacts/', contacts, name='contacts'),  # http://127.0.0.1:8000/contacts/
-    path('products/', all_products, name='all_products'),
-    path('products/<int:product_id>/', product, name='product'),
+    path('contacts', contacts, name='contacts'),  # http://127.0.0.1:8000/contacts/
+    # path('products', all_products, name='product_list'),
+    # path('products/<int:product_id>', product, name='product_detail'),
+    path('products', ProductListView.as_view(), name='product_list'),
+    path('products/<int:pk>', ProductDetailView.as_view(), name='product_detail'),
+    path('products/create/', ProductCreateView.as_view(), name='product_create'),
+    path('products/update/<int:pk>', ProductUpdateView.as_view(), name='product_update'),
+    path('products/delete/<int:pk>', ProductDeleteView.as_view(), name='product_delete'),
 ]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
